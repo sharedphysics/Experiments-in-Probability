3f1986bb58d5edda81d81a1f447806312534422d
Roman K <sharedphysics@gmail.com>
2020-03-14 20:48:53 -0400

    Added Absorption Streak Model

--- 25-percent-investment-calculator-with-absorbing-streak.py
+++ 25-percent-investment-calculator-with-absorbing-streak.py
@@ -5,6 +5,7 @@ import random
 coin = ('success', 'failure', 'failure', 'failure' )
 success, failure = 0, 0
 rounds = 0
+absorbingstreak = 0
 bank = 100000000
 print('***********************************************')
 print('****   25% Investment Success Simulation   ****')
@@ -17,6 +18,7 @@ while True:
     if user_input == 'x' or user_input == 'X':
         percentagesuccess = round(success/rounds, 2)
         percentagefailure = round(failure/rounds,2)
+        netinvestment = bank - 100000000
         totalgrowth = round(((bank/100000000-1)*100), 2)
         growthperperiod = round(((bank - 100000000)/rounds), 2)
         print(' ')
@@ -26,6 +28,7 @@ while True:
         print('  Rounds invested = {}'.format(rounds))
         print('  Orig = $100000000')
         print('  Bank = ${}'.format(bank))
+        print('  Net Income = ${}'.format(netinvestment))
         print('  Total Growth = {}'.format(totalgrowth)+ '%')
         print('  Growth Per Round = ${}'.format(growthperperiod))
         print(' ')
@@ -42,6 +45,26 @@ while True:
         print('**********************************')
         print(' ')
         break
+    if absorbingstreak == 4:
+        rounds += 1
+        failure += 1
+        netinvestment = bank - 100000000
+        percentagesuccess = round(success/rounds, 2)
+        percentagefailure = round(failure/rounds,2)
+        print(' ')
+        print('*************************************************')
+        print('  You have lost four times in a row. Game Over.')
+        print('  Rounds invested = {}'.format(rounds))
+        print('  Net income: ${}'.format(netinvestment))
+        print(' ')
+        print('  Success = {}'.format(success))
+        print('  Success Percentage = {}'.format(percentagesuccess) + '%')
+        print('  Failure = {}'.format(failure))
+        print('  Failure Percentage = {}'.format(percentagefailure) + '%')
+        print(' ')
+        print('*************************************************')
+        print(' ')
+        break
     if bank <= 10000000:
         print(' ')
         print('**********************************')
@@ -49,17 +72,21 @@ while True:
         print('**********************************')
         print(' ')
     if flip == 'success':
-        print('Successful investment. You earned $40,000,000'.
+        print(' ')
+        print('** Successful investment. You earned $40,000,000 **'.
               format(flip))
         success += 1
         rounds += 1
+        absorbingstreak = 0
         bank += 40000000
         print('  Bank = ${}'.format(bank))
         print('  Rounds invested = {}'.format(rounds))
     else:
-        print("Failed investment. You lost $10,000,000".format(flip))
+        print(' ')
+        print("** Failed investment. You lost $10,000,000 **".format(flip))
         failure += 1
         rounds += 1
+        absorbingstreak += 1
         bank -= 10000000
         print('  Bank = ${}'.format(bank))
         print('  Rounds invested = {}'.format(rounds))
